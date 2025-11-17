3554fbd4a0fb33690d232973750880a83060e76d
cleanos123 <carlosleanos1@yahoo.com>
2025-11-17 14:59:27 -0800

    Completed (sudo code) needs fixing to send index to client whe nthey connect

--- httpsetup.c
+++ httpsetup.c
@@ -1,13 +1,32 @@
-#include <sys/socket.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
 #include <pthread.h>
-#include <openssl/ssl.h>
 #include <regex.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 
 #define PORT 8080
 #define BACKLOG 10
-#define BUFFER_SIZE 1000000
+#define BUFFER_SIZE 104857600
+
+////////////////////////////////////////////////////////////
+char *url_decode(const char *src);
+const char *get_file_extension(const char *file_name);
+void* handle_client(void *arg);
+void build_http_response(const char *file_name, const char *file_ext, char *response, size_t *response_len);
+const char *get_mime_type(const char *file_ext);
+////////////////////////////////////////////////////////////
 
 int main(){
 	
@@ -29,31 +48,31 @@ int main(){
 	}
 	
 	if(listen(server_fd, BACKLOG) < 0){
-		perror("LISTEN FAILED")
+		perror("LISTEN FAILED");
 		exit(EXIT_FAILURE);
 	}
 	
 	while(1){
 		//Client connections
 		struct sockaddr_in client_addr;
-		socklent_t client_addr_len = sizeof(client_addr);
+		socklen_t client_addr_len = sizeof(client_addr);
 		int *client_fd = malloc(sizeof(int));
 		
-		if((*client_fd = accept(server_fd,(struct sockaddr *)&client_addr_len)) < 0){
+		if((*client_fd = accept(server_fd,(struct sockaddr *)&client_addr,&client_addr_len)) < 0){
 			perror("ACCEPT FAILED");
 			continue;
 		}
 		
 		pthread_t thread_id;
 		pthread_create(&thread_id, NULL, handle_client, (void *)client_fd);
-		pthread_detach
+		pthread_detach;
 		
 	}
 	
 	return 0;
 }
 
-void* handle client(void *arg){
+void* handle_client(void *arg){
 	int client_fd = *((int*)arg);
 	char *buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
 	
@@ -63,13 +82,107 @@ void* handle client(void *arg){
 		regcomp( &regex,"GET /([^ ]*) HTTP/1", REG_EXTENDED);
 		regmatch_t matches[2];
 		
-		if (regexec(&regex, buffer, 2, matches, 0)
-		
+		if (regexec(&regex, buffer, 2, matches, 0) == 0){
+			// extract filename from request and decode URL
+			buffer[matches[1].rm_eo] = '\0';
+			const char *url_encoded_file_name = buffer + matches[1].rm_so;
+			char *file_name = url_decode(url_encoded_file_name);
+			
+			//get file extension
+			char file_ext[32];
+			strcpy(file_ext, get_file_extension(file_name));
+			
+			//build http response
+			char *response = (char *)malloc(BUFFER_SIZE * 2 * sizeof(char));
+			size_t response_len;
+			build_http_response(file_name, file_ext, response, &response_len);
+			
+			//send http response
+			send(client_fd, response, response_len, 0);
+			
+			free(response);
+			free(file_name);
+		}
+		regfree(&regex);
 	}
+	close(client_fd);
+	free(arg);
+	free(buffer);
+	return NULL;
+}
+
+void build_http_response(const char *file_name, const char *file_ext, char *response, size_t *response_len){
+	//build https header
+	const char *mime_type = get_mime_type(file_ext);
+	char *header = (char *)malloc(BUFFER_SIZE * sizeof(char));
+	snprintf(header, BUFFER_SIZE, "HTTP/1.1 200 OK\r\n""Content-Type: %s\r\n""\r\n", mime_type);
 	
+	//if file not exists, repsonse 404 not Found
+	int file_fd = open(file_name, O_RDONLY);
+	if (file_fd == -1){
+		snprintf(response, BUFFER_SIZE, "HTTP/1.1 404 Not Found\r\n""Content-Type: text/plain\r\n""\r\n""404 Not Found");
+		*response_len = (strlen(response));
+		return;
+	}
+	//get file size for content length
+	struct stat file_stat;
+	fstat(file_fd, &file_stat);
+	off_t file_size= file_stat.st_size;
 	
+	//copy header to response buffer
+	*response_len = 0;
+	memcpy(response, header, strlen(header));
+	*response_len += strlen(header);
 	
+	//copy file to response buffer
+	ssize_t bytes_read;
+	while ((bytes_read = read(file_fd, response + *response_len, BUFFER_SIZE - *response_len)) > 0){
+		*response_len += bytes_read;
+	}
+	free(header);
+	close(file_fd);
+}
 	
-	
-	
+const char *get_mime_type(const char *file_ext){
+    if (strcasecmp(file_ext, "html") == 0 || strcasecmp(file_ext, "htm") == 0) {
+        return "text/html";
+    } else if (strcasecmp(file_ext, "txt") == 0) {
+        return "text/plain";
+    } else if (strcasecmp(file_ext, "jpg") == 0 || strcasecmp(file_ext, "jpeg") == 0) {
+        return "image/jpeg";
+    } else if (strcasecmp(file_ext, "png") == 0) {
+        return "image/png";
+    } else {
+        return "application/octet-stream";
+    }
+}
+
+char *url_decode(const char *src){
+    size_t src_len = strlen(src);
+    char *decoded = malloc(src_len + 1);
+    size_t decoded_len = 0;
+
+    // decode %2x to hex
+    for (size_t i = 0; i < src_len; i++) {
+        if (src[i] == '%' && i + 2 < src_len) {
+            int hex_val;
+            sscanf(src + i + 1, "%2x", &hex_val);
+            decoded[decoded_len++] = hex_val;
+            i += 2;
+        } else {
+            decoded[decoded_len++] = src[i];
+        }
+    }
+
+    // add null terminator
+    decoded[decoded_len] = '\0';
+    return decoded;
+}
+
+const char *get_file_extension(const char *file_name){
+    const char *dot = strrchr(file_name, '.');
+    if (!dot || dot == file_name) {
+        return "";
+    }
+    return dot + 1;
 }

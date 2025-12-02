9f7f92665dd982e3c8461bcc4f530daab0036bbe
cleanos123 <113729266+cleanos123@users.noreply.github.com>
2025-12-01 22:34:13 -0800

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -18,7 +18,7 @@ fi
 
 # 2. Compile the HTTPS server
 echo "[*] Compiling..."
-egcc -O2 -pthread -Wall -Wextra -o "$OUT" "$SRC" -lssl -lcrypto
+egcc -O2 -pthread -Wall -Wextra -o "$OUT" "$SRC" -lssl -lcrypto -ljpeg
 if [ $? -ne 0 ]; then
     echo "[!] Build failed."
     exit 1
